ae9e4606e9a31cc2f08844a16df7654dd576a553
SKME20417 <93089937+SKME20417@users.noreply.github.com>
2024-02-15 22:12:28 +0530

    Add files via upload

---
+++ main_projects_15_02.py
@@ -0,0 +1,12 @@
+import new_custome15_02
+
+res1 = new_custome15_02.area(5)
+print(res1)
+
+res2 = new_custome15_02.greeting('pooja')
+print(res2)
+
+new_custome15_02.perimeter()
+
+
+print("main module name :", __name__)
---
+++ new_custome15_02.py
@@ -0,0 +1,18 @@
+print("Hello how are you")
+print(10)
+print("hello worls")
+
+def greeting(name):
+    return f"Hello {name}"
+
+def area(redius):
+    return 3.14*redius*redius
+
+
+def perimeter():
+    print("i am claculating perimeter of circle")
+    
+print("custome module name:" , __name__)
+
+if __name__ == "__main__":
+    print("1000 lines of extra code and it is confidential ********")
