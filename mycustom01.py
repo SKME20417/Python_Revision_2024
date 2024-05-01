f79b22ec2417009f8e8a1305b5b09c2ffd55575d
SKME20417 <93089937+SKME20417@users.noreply.github.com>
2024-05-01 20:19:02 +0530

    Add files via upload

---
+++ mycustom01.py
@@ -0,0 +1,10 @@
+def greeting(name):
+    """This function is greenting us"""
+    print("hello good evening " +  name)
+    
+print("I am having custom functionalities...")
+
+print("custome module name : ", __name__)
+
+if __name__ == "__main__":
+    print("i have some confidential code ************")
---
+++ myproject01.py
@@ -0,0 +1,9 @@
+import mycustom01
+
+mycustom01.greeting('ravi')
+
+mycustom01.greeting('navneet')
+
+print("i am having some main project functionalities")
+
+print("main project name : " , __name__)
