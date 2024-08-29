57ea2a4e923b37a05ea162cf4a96ccf4687dcf35
SKME20417 <93089937+SKME20417@users.noreply.github.com>
2024-08-29 13:10:20 +0530

    Add files via upload

---
+++ custom_29_08.py
@@ -0,0 +1,26 @@
+print("I am a custom 29 08 module")
+
+def greeting(name):
+    print("I am greeting you in the morning", name)
+    
+def area(redius):
+    area = 2*3.14*redius
+    return area
+
+def custom_test(text):
+    print("i am messaging you")
+    
+    
+print("Today is a good weather")
+
+print("I am having some custom functionalites")
+
+print("custom module 2908:", __name__)
+
+
+# confidentail code
+if __name__ == "__main__":
+    print("This is a complete confidential code ************** ")
+
+
+
---
+++ project_29_08.py
@@ -0,0 +1,16 @@
+import custom_29_08 as myc
+
+
+print("i am a main project")
+
+myc.greeting('ravi')
+
+myc.custom_test("i am revising all the concepts")
+
+res = myc.area(5)
+print(res)
+
+print("this is complete project")
+
+print("final project 29_08:", __name__)
+
